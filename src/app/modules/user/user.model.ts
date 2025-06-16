554fbe1c97f1d4b9cc7a85e428b33072b56f6270
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-06-16 17:46:51 +0600

    user interface and model file added

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ['admin', 'user'],
+        message: '{VALUE} is not valid role',
+      },
+      default: 'user',
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
