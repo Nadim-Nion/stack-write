369c53806c609431c5d351b4605983f8caeb6fe6
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-06-18 23:29:06 +0600

    handle uncaught exception and unhandled rejection

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
+import { Server } from 'http';
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(
         `Stack Write Application is running on PORT: ${config.port} 😊`,
       );
@@ -18,3 +21,22 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`unhandledRejection is detected. Server is shutting down 😈`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`uncaughtException is detected. Server is shutting down 😈`);
+  process.exit(1);
+});
+
+// Promise.reject();
+// console.log(x);
